48a6f6242a1275c175fc29abac263d5c2f08103f
AlexisPortocarrero <56561232+AlexisPortocarrero@users.noreply.github.com>
2023-10-22 04:46:45 -0300

    coments

--- Assets/ShopManager.cs
+++ Assets/ShopManager.cs
@@ -9,7 +9,7 @@ using UnityEngine.UI;
 public class ShopManager : MonoBehaviour
 {
     
-    
+    // cosas que no se deben mover
     [Header("no movible")] 
     [SerializeField] private TMP_Text text;
     [SerializeField] private Image basePersona;
@@ -28,13 +28,15 @@ public class ShopManager : MonoBehaviour
     [SerializeField] private Button buybutton;
     [SerializeField] private Sprite close;
     [SerializeField] private Sprite open;
-
-
-
+    
+    
+    
     private InteractionSystem actual;
     private Vector3 posicionInicial;
     private Vector3 posicionCentro;
     private Vector3 posicionTeletransporte;
+    
+    // cosas que se pueden mover pero mejor no
     [Header("movible")] 
     [SerializeField] private float duracionFade = 1.0f;
     [SerializeField] private float velocidad = 1.0f;
@@ -48,12 +50,14 @@ public class ShopManager : MonoBehaviour
     private void Start()
     {
         dinero.text = ""+precioaux;
+        // setea las posiciones de donde salen y a donde van los customers, basicamente va de izquierda a derecha y luego de centro a izquierda
         posicionInicial = basePersona.rectTransform.position;
         posicionCentro = new Vector3(Screen.width / 2, posicionInicial.y, 0);
         posicionTeletransporte = new Vector3(Screen.width+100, posicionInicial.y, 0);
         StartGame();
     }
 
+    //setea los auxiliares como 0
     public void StartGame()
     {
         diasaux = 0;
@@ -61,6 +65,8 @@ public class ShopManager : MonoBehaviour
         DiasStart();
     }
 
+    // dias start es un loop: diasstart - personas start - todo el juego - personas start - dias start
+    // es como un for pero con variables auxiliares
     public void DiasStart()
     {
         if (diasaux < dias.Count)
@@ -78,6 +84,7 @@ public class ShopManager : MonoBehaviour
         }
     }
 
+    // este es el for dentro del for, si hay muchos dias, este es las personas que pasan cada dia
     public void PersonasStart()
     {
         if (personasaux < dias[diasaux].dia.Count)
@@ -92,6 +99,8 @@ public class ShopManager : MonoBehaviour
             DiasStart();
         }
     }
+    
+    // una corrutina que hace que la persona entre al medio del mapa
     private IEnumerator MoverImagenEntrante()
     {
         
@@ -111,6 +120,7 @@ public class ShopManager : MonoBehaviour
         InteraccionPersona();
     }
 
+    //este es llamado por un boton invisible dentro de la zona de texto de personas, va mostrando texto por texto
     public void InteraccionPersona()
     {
         textPanel.SetActive(true);
@@ -122,15 +132,18 @@ public class ShopManager : MonoBehaviour
         textaux++;
     }
 
+    //cuando se da click al cofre finaliza la venta, este es más complicado
     public void FinalizarVenta()
     {
         textaux = 0;
+        // desactivamos botones
         foreach (var boton in botones)
         {
             boton.interactable = false;
         }
         bool todosEnB = true;
         
+        //chequeamos si todos los valores pedidos estan dentro de la bandeja de panes
         foreach (int valorA in actual.panesAceptados)
         {
             bool encontrado = false;
@@ -150,17 +163,20 @@ public class ShopManager : MonoBehaviour
                 break;
             }
         }
+        
+        // si si estan, te muestra un good ending (string) y te aumenta o disminuye grados de moral 
         if (todosEnB)
         {
             text.text = actual.goodEnding;
-            if (actual.nobleza) // hacer algo bueno para la nobleza <--
-            {
-                s.rectTransform.Rotate(Vector3.forward, 15);
-            }
-            else
+            if (actual.nobleza) 
             {
                 s.rectTransform.Rotate(Vector3.forward, -15);
             }
+            else
+            {
+                s.rectTransform.Rotate(Vector3.forward, 15);
+            }
+            // si tiene puestos los impuestos, los suma por un for y los muestra
             if (impuestos.isOn)
             {
                 int taxes=0;
@@ -185,14 +201,15 @@ public class ShopManager : MonoBehaviour
         }
         else
         {
+            // si no los encuentra, bad ending y suma todas las taxes de la bandeja
             text.text = actual.badEnding;
             if (actual.nobleza)// hacer algo malo para la nobleza -->
             {
-                s.rectTransform.Rotate(Vector3.forward, -15);
+                s.rectTransform.Rotate(Vector3.forward, 15);
             }
             else
             {
-                s.rectTransform.Rotate(Vector3.forward, 15);
+                s.rectTransform.Rotate(Vector3.forward, -15);
             }
             if (impuestos.isOn)
             {
@@ -217,6 +234,7 @@ public class ShopManager : MonoBehaviour
             }
         }
         
+        // si el precio de la bandeja es mayor al pedido, se queda el precio maximo pedido
         if (actual.price < bandeja.price)
         {
             precioaux += actual.price;
@@ -227,6 +245,7 @@ public class ShopManager : MonoBehaviour
             precioaux += bandeja.price;
         }
 
+        //se llama al metodo para que la persona se vaya y luego se eliminan todos los objetos de la bandeja
         StartCoroutine(MoverImagenSaliente());
         
         for (int i = 0; i < bandeja.objetos.Count;)
@@ -236,6 +255,8 @@ public class ShopManager : MonoBehaviour
         }
         dinero.text = $"{precioaux}";
     }
+    
+    // la persona se va xd 
     private IEnumerator MoverImagenSaliente()
     {
         yield return new WaitForSeconds(4);
@@ -252,6 +273,8 @@ public class ShopManager : MonoBehaviour
         personasaux++;
         PersonasStart();
     }
+    
+    //cuando se termina el dia se hace un fade que te muestra que dia es y pasa al siguiente dia 
     private IEnumerator RealizarFade()
     {
         panel.alpha = 0;
@@ -280,9 +303,11 @@ public class ShopManager : MonoBehaviour
         }
 
         personasaux = 0;
+        // como se empieza el dia, se cobra el diezmo que es una actividad extra
         StartCoroutine(CobrarDiezmo(precioDiezmo));
     }
 
+    // es como una persona normal pero este te cobra el diezmo sin que puedas hacer nada, basicamente te roba
     public IEnumerator CobrarDiezmo(int costo)
     {
         float t = 0;
@@ -310,8 +335,12 @@ public class ShopManager : MonoBehaviour
             diezmo.rectTransform.position = Vector3.Lerp(posicionCentro, posicionTeletransporte, t);
             yield return null;
         }
+        // se llama al inicio del loop de nuevo
         PersonasStart();
     }
+    
+    //calcula a tiempo real las cosas que estan dentro de la bolsa para que se actualizen los precios y los sprites del cofre
+    // es como el de dinero de arriba
     private void Update()
     {
         
